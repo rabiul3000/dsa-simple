b718d1396043beaf5a92fcc1ac98becccd46cccb
rabiul3000 <mdrabiul.asia@gmail.com>
2026-05-13 19:08:45 +0600

    added remove element

---
+++ remove_element.py
@@ -0,0 +1,7 @@
+Input: nums = [0,1,3,0,4], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5,
+ with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
